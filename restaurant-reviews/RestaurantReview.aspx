b27799ffc192f6b81184c36bfe432ba4ba16329b
Alina Kurliantseva <65302512+alina-kurliantseva@users.noreply.github.com>
2020-05-15 10:15:05 -0400

    Add files via upload

---
+++ restaurant-reviews/RestaurantReview.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RestaurantReview.aspx.cs" Inherits="RestaurantReview" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Alina Kurliantseva | Restaurant Reviews</title>
+    <link rel='stylesheet' runat="server" href='App_Themes/SiteStyles.css' type='text/css' media='all'  />
+</head>
+
+<body>
+    <h2>Restaurant Reviews</h2>
+    <div class="profileForm">
+        <form runat="server">
+            <label for="drpRestaurants">Restaurants:</label>
+            <asp:DropDownList ID="drpRestaurants" runat="server" OnSelectedIndexChanged="drpRestaurants_SelectedIndexChanged" AutoPostBack="True"></asp:DropDownList>
+            <asp:Panel ID="details" runat="server">
+                <label for="txtAddress">Address:</label>
+                <asp:TextBox ID="txtAddress" runat="server" ReadOnly="true"></asp:TextBox>
+                <label for="txtSummary">Summary:</label>
+                <asp:TextBox ID="txtSummary" runat="server"></asp:TextBox>
+                <label for="drpRating">Rating:</label>
+                <asp:DropDownList ID="drpRating" runat="server">
+                    <asp:ListItem Text="1" Value="1" />
+                    <asp:ListItem Text="2" Value="2" />
+                    <asp:ListItem Text="3" Value="3" />
+                    <asp:ListItem Text="4" Value="4" />
+                    <asp:ListItem Text="5" Value="5" />
+                </asp:DropDownList>
+                <asp:Button ID="btnSaveChanges" runat="server" Text="Save Changes" OnClick="btnSaveChanges_Click" /><br />
+                <asp:Label ID="lblConfirmation" runat="server" Text=""></asp:Label>
+            </asp:Panel>
+        </form>
+    </div>
+</body>
+</html>
---
+++ restaurant-reviews/RestaurantReview.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Web.UI.WebControls;
+
+public partial class RestaurantReview : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        restaurants allRestaurants = null;
+        if (Session["allRestaurants"] == null)
+        {
+            string xmlFile = MapPath(@"~/restaurant-reviews.xml");
+            using (FileStream fs = new FileStream(xmlFile, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(restaurants));
+                allRestaurants = (restaurants)serializer.Deserialize(fs);
+            }
+            Session["allRestaurants"] = allRestaurants;
+        }
+        else
+        {
+            allRestaurants = Session["allRestaurants"] as restaurants;
+        }
+        if (!IsPostBack)
+        {
+            drpRestaurants.DataSource = allRestaurants.restaurant;
+            drpRestaurants.DataTextField = "name";
+            drpRestaurants.DataBind();
+            drpRestaurants.Items.Insert(0, new ListItem("Please select one...", "0"));
+            drpRestaurants.Visible = true;
+            details.Visible = false;
+        }
+    }
+    protected void drpRestaurants_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        restaurants allRestaurants = Session["allRestaurants"] as restaurants;
+        if (drpRestaurants.SelectedValue == "0")
+        {
+            drpRestaurants.Visible = true;
+            details.Visible = false;
+            return;
+        }
+        else
+        {
+            drpRestaurants.Visible = true;
+            ShowRestaurant(allRestaurants.restaurant[drpRestaurants.SelectedIndex - 1]);
+        }
+    }
+    protected void ShowRestaurant (restaurant r)
+    {
+        details.Visible = true;
+        txtAddress.Text = r.location.address + ", "
+                        + r.location.city + ", "
+                        + r.location.province + " "
+                        + r.location.postalCode;
+        txtSummary.Text = r.summary;
+        drpRating.SelectedValue = r.rating.Value.ToString();
+        lblConfirmation.Visible = false;
+    }
+    protected void btnSaveChanges_Click(object sender, EventArgs e)
+    {
+        restaurants allRestaurants = Session["allRestaurants"] as restaurants;
+        if (drpRestaurants.SelectedValue != "0")
+        {
+            restaurant r = allRestaurants.restaurant[drpRestaurants.SelectedIndex - 1];
+            r.summary = txtSummary.Text;
+            r.rating.Value = decimal.Parse(drpRating.SelectedValue);
+            string xmlFile = MapPath(@"~/restaurant-reviews.xml");
+            using (FileStream fs = new FileStream(xmlFile, FileMode.Create))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(restaurants));
+                serializer.Serialize(fs, allRestaurants);
+            }
+            lblConfirmation.Visible = true;
+            lblConfirmation.Text = "Revised Restaurant Review has been saved to" + xmlFile;
+        }
+    }
+}
